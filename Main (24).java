840047203250db46a5ff26524e077df5f69cb007
victoriya40 <victoriyakrishnan@gmail.com>
2025-12-11 18:24:17 +0530

    Add files via upload

---
+++ Main (24).java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        String result=(a > b && a > c) ? "A is greatest"
+                       : (b > a && b > c) ? "B is greatest"
+                       : "C is greatest";
+        System.out.println(result);
+	}
+}
