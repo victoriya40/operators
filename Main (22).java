3e9a1d27690186318c758180676c5854a7a6dbcc
victoriya40 <victoriyakrishnan@gmail.com>
2025-12-11 15:58:38 +0530

    Add files via upload

---
+++ Main (22).java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int n1=sc.nextInt();
+	    int n2=sc.nextInt();
+	    int n3=sc.nextInt();
+	    int value=(n1+n2+n3)/3;
+		System.out.println(value);
+	}
+}
